58c061ed05972a2615140cfddf59f95956b8bbf9
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-12-21 19:51:07 +0900

    [unrated] Title: 억억단을 외우자, Time: 8979.99 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
+++ 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
@@ -1,15 +1,13 @@
 def solution(e, starts):
     res = []
     num = 0
-    divisor = [0 for i in range(e + 1)]
+    div = [1] * (e + 1)
     for i in range(2, e + 1):
-        for j in range(1, min(e // i + 1, i)):
-            divisor[i * j] += 2
-    for i in range(1, int(e ** (1 / 2)) + 1):
-        divisor[i ** 2] += 1
+        for j in range(i, e + 1, i):
+            div[j] += 1
     for i in range(e, 0, -1):
-        if divisor[i] >= num:
-            num = divisor[i]
+        if div[i] >= num:
+            num = div[i]
             res.append(i)
     res = res[::-1]
     for i in range(len(starts)):
