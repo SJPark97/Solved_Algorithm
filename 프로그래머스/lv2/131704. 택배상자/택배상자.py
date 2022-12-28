bdb1034e72d6897b42b18e67a26cc2b95ce1004f
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-12-28 19:57:38 +0900

    [level 2] Title: 택배상자, Time: 292.49 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.py
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -1,25 +1,35 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
 def solution(order):
     answer = 0
-    sub_belt = []
-    n = 0
-
-    def chk_belt():
-        nonlocal answer, n
-        while sub_belt:
-            if sub_belt[-1] != order[n]:
+    stacks = []
+    N = len(order)
+    i = 1
+    idx = 0
+    while i < N+1:
+        stacks.append(i)
+        while stacks[-1] == order[idx]:
+            idx += 1
+            stacks.pop()
+            if len(stacks) == 0:
                 break
-            else:
-                sub_belt.pop()
-                answer += 1
-                n += 1
+        i += 1
 
-    for box in range(1, len(order) + 1):
-        chk_belt()
-        if box == order[n]:
-            answer += 1
-            n += 1
-            continue
-        sub_belt.append(box)
-    chk_belt()
 
-    return answer
+    return idx
