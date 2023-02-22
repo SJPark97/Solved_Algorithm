1428ac0c91a39dc5583319b3454a78976d54c64a
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2023-02-22 20:54:18 +0900

    [unrated] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -1,38 +1,53 @@
 from collections import deque
-
-
+# bfs로 풀어보자
+# 최단거리는 bfs!
 def solution(maps):
-    def bfs(lever_x, lever_y):
-        answer = 0
-        cnt = 0
-        visit = [[False] * len(maps[0]) for _ in range(len(maps))]
-        visit[lever_x][lever_y] = True
-        queue = deque([(lever_x, lever_y, 0)])
+    len_r = len(maps)
+    len_c = len(maps[0])
+    def bfs(start, end):
+        v1, v2 = start
+        queue = deque([])
+        queue.append((v1, v2, 1))
+
         while queue:
-            l_x, l_y, distance = queue.popleft()
-            for n in range(4):
-                nx, ny = l_x + dx[n], l_y + dy[n]
-                if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and not visit[nx][ny]:
-                    visit[nx][ny] = True
-                    if maps[nx][ny] == "S" or maps[nx][ny] == "E":
-                        answer += distance + 1
-                        cnt += 1
-                        if cnt == 2:
-                            return answer
-                    if maps[nx][ny] != "X":
-                        queue.append((nx, ny, distance + 1))
+            r, c, d = queue.popleft()
+
+            # 델타 검색 / 상 하 좌 우
+            # dr = [-1, 1, 0, 0]
+            # dc = [0, 0, -1, 1]
+            for dr in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr = r + dr[0]
+                nc = c + dr[1]
+                if 0 <= nr < len_r and 0 <= nc < len_c and maps[nr][nc] != "X" and visited[nr][nc] == 0:
+                    visited[nr][nc] = 1
+                    queue.append((nr, nc, d + 1))
+
+                    if (nr, nc) == end:
+                        return d
         return -1
 
-    def find_lever():
-        for i in range(len(maps)):
-            for j in range(len(maps[0])):
-                if maps[i][j] == "L":
-                    return i, j
+    # 주요 변수 구하기
+    for i in range(len_r):
+        for j in range(len_c):
+            if maps[i][j] == "S":
+                start_point = (i, j)
+            elif maps[i][j] == "E":
+                end_point = (i, j)
+            elif maps[i][j] == "L":
+                lever = (i, j)
 
-    x, y = find_lever()
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    return bfs(x, y)
+    # visited 만들기
+    visited = [[0 for _ in range(len_c)] for _ in range(len_r)]
+    # 시작지점에서 레버까지
+    to_lever = bfs(start_point, lever)
+    if to_lever == -1:
+        return -1
 
+    # visited 초기화
+    visited = [[0 for _ in range(len_c)] for _ in range(len_r)]
+    # 레버에서 종료지점까지
+    to_exit = bfs(lever, end_point)
+    if to_exit == -1:
+        return -1
+    return to_lever + to_exit
 
-print(solution(["SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"]))
