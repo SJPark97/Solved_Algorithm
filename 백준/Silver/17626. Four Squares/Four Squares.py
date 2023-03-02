5e7ec171bf8224b6cf3d261d69792f2ec38e322a
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2023-03-02 21:56:08 +0900

    [Silver III] Title: Four Squares, Time: 56 ms, Memory: 31688 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,16 @@
+n = int(input())
+sq = [0 if i**0.5%1 else 1 for i in range(n+1)]
+
+mincount = 4
+for i in range(int(n**0.5), 0, -1):
+    if sq[n] :
+        mincount=1
+        break
+    elif sq[n-i**2] :
+        mincount=2
+        break
+    else:
+        for j in range(int((n-i**2)**0.5), 0, -1):
+            if sq[(n-i**2)-j**2]:
+                mincount=3
+print(mincount)
