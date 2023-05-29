7e23eda854280d69b015880341e3a08f596c65e8
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2023-05-29 18:03:05 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 841.39 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,7 @@
+def solution(r1, r2):
+    answer = 1
+    for r in range(1, r2):
+        y1 = -(-((r1 ** 2 - r ** 2) ** 0.5) // 1) if r1 > r else 0
+        y2 = int((r2 ** 2 - r ** 2) ** 0.5)
+        answer += y2 - y1 + 1
+    return answer * 4
