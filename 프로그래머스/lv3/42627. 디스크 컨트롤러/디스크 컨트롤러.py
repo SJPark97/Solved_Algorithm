a97c73d4136fc7c401e024d379ab701805d83c87
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2023-07-17 19:38:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,24 @@
-import heapq
+from heapq import heappop, heappush
+from collections import deque
 
 
 def solution(jobs):
-    # 요청시각이 빠른 순서대로 정렬하며, 요청시각이 같을 경우에는 소요시간이 적게 걸리는 순서대로 정렬한다.
-    tasks = sorted([(x[1], x[0]) for x in jobs],
-                   key=lambda x: (x[1], x[0]), reverse=True)
-    q = []
-    heapq.heappush(q, tasks.pop())
-    current_time, total_response_time = 0, 0
-
-    # 큐(대기리스트) 에 남아있는 작업이 있을 때까지 반복문을 실행한다.
-    while len(q) > 0:
-        dur, arr = heapq.heappop(q)  # dur은 작업 소요시간, arr은 작업 요청시각
-        current_time = max(current_time + dur, arr + dur)
-        total_response_time += current_time - arr
-        while len(tasks) > 0 and tasks[-1][1] <= current_time:
-            heapq.heappush(q, tasks.pop())
-        if len(tasks) > 0 and len(q) == 0:
-            heapq.heappush(q, tasks.pop())
-    return total_response_time // len(jobs)
+    jobs.sort(key=lambda x: (x[0], x[1]))
+    jobs = [(i[1], i[0]) for i in jobs]
+    length = len(jobs)
+    jobs = deque(jobs)
+    queue = []
+    heappush(queue, jobs.popleft())
+    nowTime = 0
+    answer = 0
+    while queue:
+        workingTime, startTime = heappop(queue)
+        nowTime = workingTime + max(nowTime, startTime)
+        answer += nowTime - startTime
+        while jobs:
+            if jobs[0][1] > nowTime:
+                break
+            heappush(queue, jobs.popleft())
+        if jobs and not queue:
+            heappush(queue, jobs.popleft())
+    return answer // length
