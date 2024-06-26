f61419a0e87cc0adc7282a873141377118fd9f8d
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2024-06-26 16:05:07 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // c와의 연결을 끊어 c++의 성능을 늘리는 코드
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T, A, B;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> A >> B;
+        cout << A + B << " \n";
+    }
+    return 0;
+}
