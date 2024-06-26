3a10d5b330b68956ca8451940744fdac629ce8bc
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2024-06-26 11:06:07 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-    cout << "         ,r\'\"7" << endl;
-    cout << "r`-_   ,'  ,/" << endl;
-    cout << " \\. \". L_r'" << endl;
-    cout << "   `~\\/" << endl;
-    cout << "      |" << endl;
-    cout << "      |" << endl;
+    cout << "         ,r\'\"7\n"
+            << "r`-_   ,'  ,/\n"
+            << " \\. \". L_r'\n"
+            << "   `~\\/\n"
+            << "      |\n"
+            << "      |\n";
+    return 0;
 }
