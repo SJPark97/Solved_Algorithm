cab104edeb6456aa8bda5256950fb02998639f95
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2024-06-28 18:29:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // C와의 연결을 끊어 C++의 성능을 늘리는 코드
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string a;
+    cin >> a;
+    cout << a.length();
+    return 0;
+}
