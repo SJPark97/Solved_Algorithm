1ee433ce60fcae1c885e5f8fbffbba7adec62c23
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2025-04-02 13:33:01 +0900

    [level 2] Title: 완전범죄, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,20 +1,23 @@
 function solution(info, n, m) {
-    const sortedInfo = info.sort((a, b) => b[1] - a[1]).sort((a, b) => b[0] / b[1] - a[0] / a[1])
-                           
-    
-    let answer = -1;
-    let aTotal = 0;
-    let bTotal = 0;
-    for (info of sortedInfo) {
-        if (bTotal + info[1] < m) {
-            bTotal += info[1];
+    const itemsSortedByPriority = info
+        .sort((a, b) => b[1] - a[1])
+        .sort((a, b) => b[0] / b[1] - a[0] / a[1]);
+
+    let aTraceSum = 0;
+    let bTraceSum = 0;
+
+    for (const item of itemsSortedByPriority) {
+        const [aTrace, bTrace] = item;
+        if (bTraceSum + bTrace < m) {
+            bTraceSum += bTrace;
         } else {
-            aTotal += info[0];
-        };
-    };
-    if (aTotal < n && bTotal < m) {
-        answer = aTotal;
-    };
-    
-    return answer;
-};
+            aTraceSum += aTrace;
+        }
+    }
+
+    if (aTraceSum < n && bTraceSum < m) {
+        return aTraceSum;
+    }
+
+    return -1;
+}
