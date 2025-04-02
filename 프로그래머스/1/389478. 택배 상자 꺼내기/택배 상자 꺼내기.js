0de7b1a3b735526353f41bd841f9eae523b02724
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2025-04-02 11:00:40 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,24 +1,25 @@
 function solution(n, w, num) {
-    const boxInfo = Array.from({ length: w }, () => []);
-
-    boxInfo.forEach((arry, idx) => {
-        let startNumber = idx + 1;
-        const plusArray = [2 * w + 1 - 2 * startNumber, 2 * startNumber - 1];
-        let isOdd = true;
-        while (startNumber <= n) {
-            arry.push(startNumber);
-            if (isOdd) {
-                startNumber += plusArray[0];
-            } else {
-                startNumber += plusArray[1];
-            };
-            isOdd = !isOdd;
+    const floor = Math.ceil(num / w);
+    let isOdd = floor % 2 === 1;
+    let startNumber = 0;
+    if (isOdd) {
+        startNumber = (num - 1) % w + 1;
+    } else {
+        startNumber = w - (num - 1) % w
+    }
+    const plusArray = [2 * w + 1 - 2 * startNumber, 2 * startNumber - 1];
+    let answer = 1;
+    let boxNumber = num;
+    while (boxNumber <= n) {
+        if (isOdd) {
+            boxNumber += plusArray[0];
+        } else {
+            boxNumber += plusArray[1];
         };
-    });
-    
-    const resultArray = boxInfo.filter((arry) => arry.filter((number) => number === num).length > 0)
-                               .flatMap((value) => value);
-    const answer = resultArray.length - resultArray.findIndex(number => number === num);
-    
+        isOdd = !isOdd;
+        if (boxNumber <= n) {
+            answer += 1
+        }
+    };
     return answer;
 };
