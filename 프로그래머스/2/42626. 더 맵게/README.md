fc0fb6ab586cd935601dcd83b5b77b0829154ef3
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2025-04-24 10:53:05 +0900

    [level 2] Title: 더 맵게, Time: 432.19 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 612.41 ms
+메모리: 109 MB, 시간: 432.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 17:55:57
+2025년 04월 24일 10:53:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,55 +1,52 @@
 function solution(scoville, K) {
-  const sortedScoville = [...scoville].sort((a, b) => a - b); // 원본 보존
-  let sortedScovilleIdx = 0;
-
-  const queue = [];
-  let queueIdx = 0;
-
-  let answer = 0;
-
-  while (true) {
-    const candidates = [];
-
-    // 2개 뽑기
-    for (let i = 0; i < 2; i++) {
-      const s = sortedScovilleIdx < sortedScoville.length
-        ? sortedScoville[sortedScovilleIdx]
-        : Infinity;
-      const q = queueIdx < queue.length
-        ? queue[queueIdx]
-        : Infinity;
-
-      if (s <= q) {
-        candidates.push(s);
-        sortedScovilleIdx++;
-      } else {
-        candidates.push(q);
-        queueIdx++;
-      }
+    const sortedScoville = scoville.sort((a, b) => a - b);
+    let sortedScovilleIdx = 0;
+    
+    const queue = [];
+    let queueIdx = 0;
+    
+    let answer = 0;
+    
+    while (true) {
+        let num1;
+        let num2;
+        
+        for (let i = 0; i < 2; i++) {
+            let smallNum;
+            if (sortedScoville.length > sortedScovilleIdx && queue.length > queueIdx) {
+                if (sortedScoville[sortedScovilleIdx] < queue[queueIdx]) {
+                    smallNum = sortedScoville[sortedScovilleIdx];
+                    sortedScovilleIdx++;
+                } else {
+                    smallNum = queue[queueIdx];
+                    queueIdx++;
+                }
+            } else if (sortedScoville.length > sortedScovilleIdx) {
+                smallNum = sortedScoville[sortedScovilleIdx];
+                sortedScovilleIdx++;
+            } else if (queue.length > queueIdx) {
+                smallNum = queue[queueIdx];
+                queueIdx++;
+            }
+            
+            if (i === 0) {
+                num1 = smallNum;
+            } else {
+                num2 = smallNum;
+            }
+        }
+        if (num1 >= K && num2 >= K) {
+            break;
+        } else if (num2 === undefined && num1 >= K) {
+            break;
+        } else if (num2 === undefined && num1 < K) {
+            answer = -1;
+            break;
+        }
+        const newNumber = num1 + num2 * 2;
+        queue.push(newNumber);
+        answer++;
     }
-
-    const [num1, num2] = candidates;
-
-    if (num1 === Infinity) {
-      // 두 개 다 없음
-      answer = -1;
-      break;
-    }
-    if (num2 === Infinity) {
-      // 하나만 있음
-      if (num1 >= K) break;
-      else {
-        answer = -1;
-        break;
-      }
-    }
-
-    if (num1 >= K && num2 >= K) break;
-
-    const newScoville = num1 + num2 * 2;
-    queue.push(newScoville);
-    answer++;
-  }
-
-  return answer;
+    
+    return answer;
 }
