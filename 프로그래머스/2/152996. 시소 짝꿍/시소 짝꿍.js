c0a5b490f751c1344a812e23cbd5923d093793a1
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2025-05-12 14:15:04 +0900

    [level 2] Title: 시소 짝꿍, Time: 14.03 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,22 +1,31 @@
 function solution(weights) {
     const weightsMap = new Map();
     const weightRatio = [3/2, 2, 2/3, 4/3, 1/2, 3/4];
+
+    // 1. 무게별 인원 수 기록
     for (const weight of weights) {
         weightsMap.set(weight, (weightsMap.get(weight) || 0) + 1);
     }
-    
+
     let answer = 0;
+
     for (const [w, cnt] of weightsMap) {
+        // 2. 동일 무게끼리 짝꿍
         if (cnt >= 2) {
-            answer += cnt * (cnt - 1) / 2;
+            answer += (cnt * (cnt - 1)) / 2;
         }
+
+        // 3. 다른 무게와 비율로 짝꿍
         for (const ratio of weightRatio) {
             const pairWeight = w * ratio;
-            if (weightsMap.get(pairWeight)) {
+            if (Number.isInteger(pairWeight) && weightsMap.get(pairWeight)) {
                 answer += weightsMap.get(pairWeight) * cnt;
             }
         }
+
+        // 4. 중복 방지: 현재 무게 삭제
         weightsMap.delete(w);
     }
+
     return answer;
 }
