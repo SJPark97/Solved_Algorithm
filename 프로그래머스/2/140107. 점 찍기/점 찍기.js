460666b2b65e95aeb0774df0325476f506d7ead4
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2025-05-20 10:59:28 +0900

    [level 2] Title: 점 찍기, Time: 10.28 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -2,7 +2,7 @@ function solution(k, d) {
     let answer = 0;
     let x = 0;
     while (x <= d) {
-        const maxY = Math.floor((d ** 2 - x ** 2) ** 0.5);
+        const maxY = Math.floor(Math.sqrt(d ** 2 - x ** 2));
         answer += Math.floor(maxY / k) + 1;
         x += k
     }
